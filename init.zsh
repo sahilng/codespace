47da191849b1c6122ec2071090f14bf28cf3a6a5
Sahil Gupta <sahilng1997@gmail.com>
2023-08-26 02:05:45 -0400

    better init

--- init.zsh
+++ init.zsh
@@ -1,13 +1,31 @@
 #!/bin/zsh
 
-# The below init script will require that you fork this repository, change my username to yours in the line below, and install https://www.github.com/sahilng/pingpong
+# Exit script on any command failure
+set -e
 
-var=$(gh cs create -R sahilng/codespace -b pong-server -m basicLinux32gb)
+# Description:
+# This script sets up a GitHub Codespace and then runs the 'pingpong' application on it.
+# Requirements:
+# - Fork this repository and replace my username with yours in the REPO assignment
+# - GitHub CLI (gh) installed and authenticated
+# - pingpong: https://www.github.com/sahilng/pingpong
 
-gh cs ports visibility 3000:public -c $var
+REPO=${REPO:-sahilng/codespace}              # Default to 'sahilng/codespace' if REPO is not set
+BRANCH=${BRANCH:-pong-server}                # Default to 'pong-server' if BRANCH is not set
+MACHINE_TYPE=${MACHINE_TYPE:-basicLinux32gb} # Default to 'basicLinux32gb' if MACHINE_TYPE is not set
+PORT=${PORT:-3000}                           # Default port
 
-output=$(gh cs ports -q '.[0].browseUrl' -c $var --json browseUrl)
+# Create a new Codespace and store its ID
+codespace_id=$(gh cs create -R $REPO -b $BRANCH -m $MACHINE_TYPE)
 
-echo "Querying Public URL: $output"
+# Make the desired port public
+gh cs ports visibility $PORT:public -c $codespace_id
 
-pingpong $output
+# Get the public URL for the Codespace
+public_url=$(gh cs ports -q '.[0].browseUrl' -c $codespace_id --json browseUrl)
+echo "Querying Public URL: $public_url"
+
+# Start the pingpong application
+pingpong $public_url
+
+echo "Your pong-server is now running. Use 'gh cs delete' to delete the Codespace if not in use to avoid charges."
