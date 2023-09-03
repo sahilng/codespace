2aa8055fdaa693e7c24007f78c91b00999903fe9
Sahil Gupta <sahilng1997@gmail.com>
2023-09-03 02:17:56 +0000

    some working changes

--- server.sh
+++ server.sh
@@ -3,8 +3,8 @@
 # Set password
 PASSWORD="test"
 
-# Generate a hashed password using IPython
-HASHED_PASSWORD=$(python -c "from IPython.lib import passwd; print(passwd('${PASSWORD}'))")
+# Generate a hashed password using notebook.auth
+HASHED_PASSWORD=$(python -c "from jupyter_server.auth import passwd; print(passwd('${PASSWORD}'))")
 
 # Start Jupyter notebook with the hashed password and other configurations
 jupyter notebook --no-browser --port=8888 --NotebookApp.password="${HASHED_PASSWORD}" --NotebookApp.allow_origin='*'
